a1917a36eadc71916a8861c7f76e26ebbcccb14c
maskaa22 <kyrosika565@gmail.com>
2025-01-25 18:25:12 +0200

    burger

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,54 @@
-import "./Header.module.css";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoIosCloseCircleOutline } from "react-icons/io";
+import c from "./Header.module.css";
+import { useState } from "react";
 
 export default function Header() {
-  return <></>;
+  const [isOpen, setIsOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(false);
+
+  const menuItems = [
+    "Home",
+    "About",
+    "Skills",
+    "Education",
+    "Experience",
+    "Projects",
+    "Contact",
+  ];
+
+  return (
+    <div className="container">
+      <header className={c.header}>
+        <div className={c.logo}>
+          <span>M</span>ariia <span>S</span>kydan
+        </div>
+        <RxHamburgerMenu
+          className={c.burger}
+          onClick={() => setIsOpen(!isOpen)}
+        />
+        <div className={`${c.menu} ${isOpen ? c.open : ""}`}>
+          <IoIosCloseCircleOutline
+            className={c.close}
+            onClick={() => setIsOpen(!isOpen)}
+          />
+          <nav>
+            <ul className={c.list}>
+              {menuItems.map((item, index) => (
+                <li
+                  key={index}
+                  className={`${c.item} ${
+                    activeItem === index ? c.active : ""
+                  }`}
+                  onClick={() => setActiveItem(index)}
+                >
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </div>
+  );
 }
