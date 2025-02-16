de17c898956f60864049bf9bb77d0cf3d5c52029
maskaa22 <kyrosika565@gmail.com>
2025-02-16 19:06:38 +0200

    section - about me

---
+++ src/components/about/About.jsx
@@ -0,0 +1,47 @@
+import c from "./About.module.css";
+import { IoMdCheckmarkCircleOutline } from "react-icons/io";
+
+export default function About() {
+  return (
+    <section className={c.about}>
+      <div className="container">
+        <div className={c.inner}>
+          <div className={c.foto}>
+            <picture>
+              <source
+                srcSet="
+                ./about/about-x1.png    1x,
+                ./about/about-x1.png 2x
+              "
+              />
+              <img src="./about/about-x1.png" alt="Mariia Skydan" />
+            </picture>
+          </div>
+          <div className={c.info}>
+            <h2 className={c.title}>About me</h2>
+            <p className={c.text}>
+              Lorem ipsum dolor sit amet consectetur. Tristique amet sed massa
+              nibh lectus netus in. Aliquet donec morbi convallis pretium.
+              Turpis tempus pharetra
+            </p>
+            <ul className={c.list}>
+              <li className={c.item}> <IoMdCheckmarkCircleOutline  className={c.icon}/>
+                Develop highly interactive Front end <span>/ User Interfaces for the
+                web</span>
+              </li>
+              <li className={c.item}><IoMdCheckmarkCircleOutline  className={c.icon}/>
+                Progressive Web Applications ( PWA ) <span>in normal and SPA Stacks</span>
+              </li>
+              <li className={c.item}><IoMdCheckmarkCircleOutline  className={c.icon}/>
+                Integration of third party services such as AWS <span>/ Digital Ocean</span>
+              </li>
+              <li className={c.item}><IoMdCheckmarkCircleOutline  className={c.icon}/>
+                Integration of payment services <span>such as M-Pesa and paypal etc</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,10 +1,12 @@
 import "./Main.module.css";
 import Hero from "../hero/Hero";
+import About from "../about/About";
 
 export default function Main() {
   return (
     <>
       <Hero />
+      <About />
     </>
   );
 }
