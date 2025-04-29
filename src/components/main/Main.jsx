5330b5ac37059ede907c70f80ce9d66ffb05a295
Mariia Skydan <kyrosika565@gmail.com>
2025-04-29 21:21:37 +0300

    form talk

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -4,6 +4,7 @@ import About from "../about/About";
 import Skills from "../skills/Skills";
 import Educations from "../educations/Educations";
 import Projects from "../projects/Projects";
+import Talk from "../talk/Talk";
 
 export default function Main() {
   return (
@@ -13,6 +14,7 @@ export default function Main() {
       <Skills />
       <Educations/>
       <Projects />
+      <Talk />
     </>
   );
 }
