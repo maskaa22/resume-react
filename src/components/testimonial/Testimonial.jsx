39c2c0f2a8e4ca0b97ffd4a05d681fa81bb4e21f
Mariia Skydan <kyrosika565@gmail.com>
2025-04-29 20:18:45 +0300

    del testimonial

--- src/components/testimonial/Testimonial.jsx
+++
@@ -1,5 +0,0 @@
-import "./Testimonial.module.css";
-
-export default function Testimonial() {
-  return <></>;
-}
--- src/components/testimonials/Testimonials.jsx
+++
@@ -1,5 +0,0 @@
-import "./Testimonials.module.css";
-
-export default function Testimonials() {
-  return <></>;
-}
